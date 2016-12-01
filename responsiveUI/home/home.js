8e9ac4a16abb2263a23c721c4659088a5790208e
Matti Ojala <ojalam1@aalto.fi>
2016-12-01 11:10:50 +0200

    Testing more buttons. Now with front and back doors!

--- responsiveUI/home/home.js
+++ responsiveUI/home/home.js
@@ -9,19 +9,70 @@ angular.module('smartHouseApp.home', ['ngRoute'])
   });
 }])
 
+/*
 .controller('HomeCtrl', [function() {
 
 }]);
+*/
+
+
+
+.controller('HomeCtrl',['$scope', function($scope){
+
+  //$scope.bool = true;
+  $scope.lockAll = true;
+  $scope.lockFront = true;
+  $scope.lockBack = true;
+
+  $scope.testDoors = function(door) {
+    if(door == "allDoors") {
+      if($scope.lockAll){
+        $scope.lockAll = false;
+        $scope.lockFront = false;
+        $scope.lockBack = false;
+      }
+      else {
+        $scope.lockAll = true;
+        $scope.lockFront = true;
+        $scope.lockBack = true;
+      }
+    }
+    else if(door == "frontDoor") {
+      if($scope.lockFront){
+        $scope.lockAll = false;
+        $scope.lockFront = false;
+      }
+      else {
+        $scope.lockAll = true;
+        $scope.lockFront = true;
+      }
+    }
+    else if(door == "backDoor") {
+      if($scope.lockBack){
+        $scope.lockAll = false;
+        $scope.lockBack = false;
+      }
+      else {
+        $scope.lockAll = true;
+        $scope.lockBack = true;
+      }
+    }
+  }
+
+}]);
+
+
+
 
 function lockDoors(state) {
   // State value changes AFTER this script is run so we are looking at previous state here
   if (state == "open") {
     document.getElementById('lockAllDoors').name = "locked"
-    alert("Locking all the doors");
+    //alert("Locking all the doors");
   }
   else {
     document.getElementById('lockAllDoors').name = "open"
-    alert("Unlocking all the doors")
+    //alert("Unlocking all the doors")
   }
 }
 
@@ -39,5 +90,4 @@ function turnLights(button) {
     document.getElementById("turnLightsOff").className="btn btn-default disabled"
     document.getElementById("turnLightsOff").childNodes[0].nodeValue="Lights are off"
   }
-
 }
